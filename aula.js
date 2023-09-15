a0531c2a1299b16feabbea784e3c82d18953e3d7
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-15 11:47:08 -0300

    atualização de arquivos

--- aula.js
+++ aula.js
@@ -1,1 +1,19 @@
+// criando uma alert em JavaScript
 alert("Olá Mundo!");
+
+/*
+    *************************
+    - Adicionando JavaScript
+    - Criando um novo projeto
+    *************************
+*/
+
+// adicionando uma mensagem na tela
+document.write('Ola o caralho to puto!');
+
+//adicione console.log
+console.log("Vai tomar no cu! Fiquei puto! Quero esfaquear uma velinha agora tbm vai ter um mendigo no meio");
+
+//Criando uma variavel
+let mensagem = "Vai se fuder seu cuzão"
+console.log(mensagem);
