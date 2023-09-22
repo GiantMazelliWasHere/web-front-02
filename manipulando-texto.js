be1bef9c4fee97822bf6de6ec819f98c324c749a
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-22 11:50:36 -0300

    novas files de JavaScript

---
+++ manipulando-texto.js
@@ -0,0 +1,11 @@
+let numFloat = 123.543;
+let linguagens = ["Java", "C#", "Python"];
+let txt = "Estão chegando as provas";
+
+// console.log(typeof numFloat.toString());
+// console.log(typeof String(numFloat));
+// console.log(typeof linguagens);
+
+// console.log(linguagens[1]);
+
+console.log(txt.replace("provas", "Vans do FBI"))
---
+++ tipos-dados.js
@@ -0,0 +1,16 @@
+//Números Inteiros
+let idadeAluno = 24;
+
+//Números flutuantes
+let valorProduto = 5.35;
+
+//String
+let nomeAluno = "Eduardo Mazelli";
+
+//Boolean
+let casado = true;
+
+// console.log(typeof idadeAluno);
+// console.log(typeof valorProduto);
+// console.log(typeof nomeAluno);
+console.log(casado);
