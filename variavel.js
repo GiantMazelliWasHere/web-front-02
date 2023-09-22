148842e68dd587df203b66d83f4b44c3dcd5e774
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-22 11:08:35 -0300

    nova file js

---
+++ variavel.js
@@ -0,0 +1,10 @@
+// Cria uma varíavel com valor indefinido
+let nome;
+
+// Cria uma variavel vazia
+let disciplina = null;
+
+// Cria ums variavel com valor
+let texto = "Este não é um exemplo de pessoa boa";
+
+console.log(texto)
