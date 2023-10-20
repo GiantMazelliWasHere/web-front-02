191d48ffafd7add1e45c929988438c63d862d1b8
Logon Aluno <logonrmlocal@fiap.com.br>
2023-10-20 11:41:08 -0300

    É ISSO AI PESSOAL

---
+++ arrray-news.js
@@ -0,0 +1,5 @@
+const times = ["Los Angeles Dodgers", "Golden Satate Warriors", "New England Patriots"];
+
+times.push("New York Yankees")
+
+console.log(times)
---
+++ exercicio.js
@@ -0,0 +1,3 @@
+for(let i = 1 ; i <= 10; i++){
+    console.log(`Esse é a ${i}ª vez que está frase é exibida no console`)
+}
