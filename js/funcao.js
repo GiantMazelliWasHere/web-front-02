ec153be2d8cb9a720445feff2d781c7915381193
Logon Aluno <logonrmlocal@fiap.com.br>
2023-10-27 10:52:43 -0300

    addendo as coisas feitas

---
+++ js/funcao.js
@@ -0,0 +1,20 @@
+function teste() {
+    console.log("Hello")
+}
+
+// teste();
+
+function meuNome(name) {
+    console.log(`Ola Filha da puta chamado ${name}`)
+}
+
+// meuNome("Jason");
+// meuNome("Michael");
+// meuNome("Freddy");
+
+function calc(arg1, arg2) {
+    return arg1 + arg2
+}
+
+// console.log(calc(4, 4))
+// console.log(calc(50000, 1000000))
